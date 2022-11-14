36c4c8e82a48899855bbbfd380dc04e1ec389bab
Joseph Yoon <jyoonopop@gmail.com>
2022-11-14 10:40:50 -0500

    user password check

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -4,11 +4,21 @@ const bcrypt = require('bcrypt')
 
 const { User } = db
 
+  
 router.post('/', async (req, res) => {
+    
     let user = await User.findOne({
-        where: { email:req.body.email }
+        where: { email: req.body.email }
     })
-    console.log(user)
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
+    } else {
+        res.json({ user })
+    }
 })
+
 
 module.exports = router
