a47cef9d677d4a195fd9d5b10d565c0e6323b503
Joseph Yoon <jyoonopop@gmail.com>
2022-11-15 08:48:47 -0500

    retrieve a user object based on jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -23,16 +23,32 @@ router.post('/', async (req, res) => {
 
 router.get('/profile', async (req, res) => {
     try {
-        let user = await User.findOne({
-            where: {
-                userId: id
-            }
-        })
-        res.json(user)
+        // Split the authorization header into [ "Bearer", "TOKEN" ]:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // Only handle "Bearer" authorization for now 
+        //  (we could add other authorization strategies later):
+        if (authenticationMethod == 'Bearer') {
+
+            // Decode the JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Get the logged in user's id from the payload
+            const { id } = result.value
+
+            // Find the user object using their id:
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(user)
+        }
     } catch {
         res.json(null)
     }
 })
+
 
 
 module.exports = router
